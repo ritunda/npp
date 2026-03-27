5ad193df207ea14221c63f8ca8f103c758378f95
ritunda <83491679+ritunda@users.noreply.github.com>
2026-03-27 05:32:26 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'nurseprep-v1';
+const urlsToCache = [
+  '/npp',
+  '/npp/index.html',
+  '/npp/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
